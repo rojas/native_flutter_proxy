935bfe1e2d694aa26c7612d0e85732788c6fbd8e
Rogerio Shimizu <roja@bunker79.com>
2024-11-01 14:10:25 -0300

    chore: remove deprecated Android plugin calls

--- android/src/main/kotlin/com/victorblaess/native_flutter_proxy/FlutterProxyPlugin.kt
+++ android/src/main/kotlin/com/victorblaess/native_flutter_proxy/FlutterProxyPlugin.kt
@@ -1,31 +1,18 @@
 package com.victorblaess.native_flutter_proxy
 
+import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.util.*
 
 /** FlutterProxyPlugin */
 public class FlutterProxyPlugin : FlutterPlugin, MethodCallHandler {
 
     private var mMethodChannel: MethodChannel? = null;
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val instance = FlutterProxyPlugin()
-            instance.onAttachedToEngine(registrar.messenger());
-        }
-    }
-
-    private fun onAttachedToEngine(messenger: BinaryMessenger) {
-        mMethodChannel = MethodChannel(messenger, "native_flutter_proxy")
-        mMethodChannel!!.setMethodCallHandler(this)
-    }
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         mMethodChannel = MethodChannel(binding.binaryMessenger, "native_flutter_proxy")
